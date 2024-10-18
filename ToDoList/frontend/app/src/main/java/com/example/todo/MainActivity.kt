61404fef34114448d4cd2a57e4ffe1aa9368ecc0
Stefan Vucicevic <stefanvucicevic002@gmail.com>
2024-10-18 21:52:06 +0200

    Test commit

--- ToDoList/frontend/app/src/main/java/com/example/todo/MainActivity.kt
+++ ToDoList/frontend/app/src/main/java/com/example/todo/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ToDoTheme {
-        Greeting("Android")
+        Greeting("Drasko")
     }
 }
