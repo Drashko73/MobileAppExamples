ce63c34d9586796fe39381adb98217906d5ae36d
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2024-10-22 00:52:00 +0200

    Revert "Added initial Backend structure"
    This reverts commit cf866ceb9b1475b2655ba28d152561617afe182d.

--- RealTimeChat/frontend/app/src/main/java/com/app/chat/data/model/Message.kt
+++
@@ -1,8 +0,0 @@
-package com.app.chat.data.model
-
-data class Message (
-    val id: String,
-    val sender: String,
-    val content: String,
-    val timestamp: Long
-)
--- RealTimeChat/frontend/app/src/main/java/com/app/chat/ui/chat/MessageItem.kt
+++
@@ -1,4 +0,0 @@
-package com.app.chat.ui.chat
-
-class MessageItem {
-}
